ea04c658073c086928ec4fcce7f5e3b9385a7811
Wayne <wayne.tr.1711@gmail.com>
2026-01-27 15:51:40 +0700

    hoang hai admin

--- src/config/index.js
+++ src/config/index.js
@@ -53,12 +53,12 @@ export const KV_KEYS = {
 // Admin users (bypass credit limits)
 export const ADMIN_USERS = ['admin', 'vixx-admin',
   '@instagram_1535273395:matrix.lvbrd.xyz', // lovefish49
+  '@instagram_1340551653:matrix.lvbrd.xyz', // ngxhoanghai
 ];
 
 // Premium users (200 credits)
 export const PREMIUM_USERS = [
   '@instagram_421229918:matrix.lvbrd.xyz', // dtran1004
-  '@instagram_1340551653:matrix.lvbrd.xyz', // ngxhoanghai
   '@instagram_7730134120:matrix.lvbrd.xyz', // minh.lt_
   '@instagram_3487576414:matrix.lvbrd.xyz', // dkieeuu
   '@instagram_56911609594:matrix.lvbrd.xyz', // vedup.1711
